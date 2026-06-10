d3a4bdc0378081e864b3a29438b17abecfb637b1
safwan <safwanemad2002@gmail.com>
2026-06-10 15:25:09 +0300

    chore: change telegram chat id to group chat

--- telegram-config.js
+++ telegram-config.js
@@ -2,5 +2,5 @@
 // Bot created via @BotFather - sends filled PDFs to a fixed group/channel
 const TELEGRAM_CONFIG = {
 	botToken: "8687576172:AAHWtE0f7qRLLWhRlARSaJJ9DJvMzzChPPo",
-	chatId: "5721989313",
+	chatId: "-5151371510",
 };
